c95b4c2e22c31475fd040aa311cbe4b359427745
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-05-30 18:00:02 +0900

    scaffold Area & Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :areas
+  resources :categories
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   resources :plans do
     resources :bookings ,:only => [:new, :create]
