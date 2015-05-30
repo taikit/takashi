bdfe8142033c81e4526d38810b7c58cf860291b9
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-05-31 00:30:14 +0900

    add day_id to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530143444) do
+ActiveRecord::Schema.define(version: 20150530152942) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150530143444) do
     t.string   "card_fingerprint"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "day_id"
   end
 
   create_table "categories", force: :cascade do |t|
