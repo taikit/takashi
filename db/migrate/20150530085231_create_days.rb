7b292a3ddfa32796515c84f6baf3f01ea12ec88e
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-05-30 17:55:37 +0900

    fix merge file

--- db/migrate/20150530085231_create_days.rb
+++ db/migrate/20150530085231_create_days.rb
@@ -1,7 +1,7 @@
 class CreateDays < ActiveRecord::Migration
   def change
     create_table :days do |t|
-      t.integer :paln_id
+      t.integer :plan_id
       t.datetime :met_at
 
       t.timestamps null: false
