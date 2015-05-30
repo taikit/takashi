06303bc4c7f7698564bd7003a1ee4d0b6ab60807
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-05-30 23:36:43 +0900

    add prfile_image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530125813) do
+ActiveRecord::Schema.define(version: 20150530143444) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "name"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150530125813) do
     t.datetime "updated_at",                          null: false
     t.string   "token"
     t.string   "facebook_url"
+    t.string   "profile_image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
